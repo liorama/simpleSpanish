59598756cc95dee2b6b7243141a8432383bdbca4
Lior Aviram <lior.aviram@gmail.com>
2019-05-14 13:24:15 +0300

    testing out some converted HTML files

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
 	StyleSheet,
-	// Text,
+	Text,
 	View,
 	Platform,
 	// AsyncStorage,
-	WebView
-	} from "react-native";
-import { Constants } from "expo";
-import ScrollableTabView, { ScrollableTabBar } from "react-native-scrollable-tab-view";
+	WebView,
+	Button
+} from "react-native";
+import {Constants} from "expo";
+import ScrollableTabView, {ScrollableTabBar} from "react-native-scrollable-tab-view";
 
 
 //function setInStorage(key, value) {
@@ -26,63 +27,86 @@ import ScrollableTabView, { ScrollableTabBar } from "react-native-scrollable-tab
 //	}
 //}
 
+/*  Trying to make the /htmltests/chapter-2/chapter-2.html format work (from zamzar.com) instead the newhtml/chapter_3/index.html
+Seems like the meta tag makes it much better. need to try putting initial scale at 0.8 and centering it.
 
+ */
 export default class simpleSpanish extends Component {
 	constructor(props) {
 		super(props);
-		this.pages = [
+		const pages = this.pages = [
 			// {title: "אינדקס", file: require("./assets/newhtml/index/index.html")},
 			// {title: "הקדמה", file: require("./assets/newhtml/introduction/index.html")},
-			{title: "פרק 1", file: require("./assets/newhtml/chapter_1/index.html")},
-			{title: "פרק 2", file: require("./assets/newhtml/chapter_2/index.html")},
-			{title: "פרק 3", file: require("./assets/newhtml/chapter_3/index.html")},
-			{title: "פרק 4", file: require("./assets/newhtml/chapter_4/index.html")},
-			{title: "פרק 5", file: require("./assets/newhtml/chapter_5/index.html")},
-			{title: "פרק 6", file: require("./assets/newhtml/chapter_6/index.html")},
-			{title: "פרק 7", file: require("./assets/newhtml/chapter_7/index.html")},
-			// {title: "פרק 8", file: require("./assets/newhtml/chapter_8/index.html")},
-			// {title: "פרק 9", file: require("./assets/newhtml/chapter_9/index.html")},
-			// {title: "פרק 10", file: require("./assets/newhtml/chapter_10/index.html")},
-			// {title: "פרק 11", file: require("./assets/newhtml/chapter_11/index.html")},
-			// {title: "פרק 12", file: require("./assets/newhtml/chapter_12/index.html")},
-			// {title: "פרק 13", file: require("./assets/newhtml/chapter_13/index.html")},
-			// {title: "פרק 14", file: require("./assets/newhtml/chapter_14/index.html")},
-			// {title: "פרק 15", file: require("./assets/newhtml/chapter_15/index.html")},
-			// {title: "מילון", file: require("./assets/newhtml/dictionary/index.html")},
-			// {title: "רשימת אלפבית", file: require("./assets/newhtml/alphabet_list/index.html")},
-			// {title: "תוספות", file: require("./assets/newhtml/apendix/index.html")},
-			// {title: "עטיפה אחורית", file: require("./assets/newhtml/back_cover/index.html")},
+			{title: "פרק 1", file: require("./assets/htmltests/chapter-1/chapter-1.html")},
+			{title: "פרק 2", file: require("./assets/htmltests/chapter-2.html")},
+			{title: "פרק 3", file: require("./assets/htmltests/chapter-5.html")},
+			{title: "פרק 4", file: require("./assets/htmltests/chapter-6.html")},
+			{title: "פרק 5", file: require("./assets/htmltests/chapter-5.html")},
+			{title: "פרק 6", file: require("./assets/htmltests/chapter-6.html")},
+			{title: "פרק 7", file: require("./assets/htmltests/chapter-7.html")},
+			{title: "פרק 8", file: require("./assets/htmltests/chapter-8.html")},
+			{title: "פרק 9", file: require("./assets/htmltests/chapter-9.html")},
+			{title: "פרק 10", file: require("./assets/newhtml/chapter_10/index.html")},
+			{title: "פרק 11", file: require("./assets/newhtml/chapter_11/index.html")},
+			{title: "פרק 12", file: require("./assets/newhtml/chapter_12/index.html")},
+			{title: "פרק 13", file: require("./assets/newhtml/chapter_13/index.html")},
+			{title: "פרק 14", file: require("./assets/newhtml/chapter_14/index.html")},
+			{title: "פרק 15", file: require("./assets/newhtml/chapter_15/index.html")},
+			{title: "מילון", file: require("./assets/newhtml/dictionary/index.html")},
+			{title: "רשימת אלפבית", file: require("./assets/newhtml/alphabet_list/index.html")},
+			{title: "תוספות", file: require("./assets/newhtml/apendix/index.html")},
+			{title: "עטיפה אחורית", file: require("./assets/newhtml/back_cover/index.html")},
 		];
 		this.state = {
-			initialTab: Object.keys(this.pages).length - 1,
+			index: 0,
 		};
 	}
 
 	render() {
-		const tabbarStyles = [styles.tabbar];
-		if (Platform.OS === "android") {
-			tabbarStyles.push(styles.androidTabbar);
+		let nextPage = this.state.index + 1; // todo: this seems to work only from chapter 1 to 2.
+		if (nextPage >= this.pages.length) {
+			nextPage = 0;
 		}
-		const tabs = [];
+		let previousPage = this.state.index - 1;
+		if (previousPage <= 0) {
+			previousPage = this.pages.length - 1;
+		}
 		return (
-			<View style={styles.container}>
-				<ScrollableTabView
-					prerenderingSiblingsNumber={0}
-					initialPage={this.state.initialTab}
-					renderTabBar={() => <ScrollableTabBar />}>
-					{
-						this.pages.map(function(pageData, index) {
-							return <WebView
-								key={index}
-								tabLabel={pageData.title}
-								source={pageData.file}
-								style={styles.webview}>
-							</WebView>
-						}, this).reverse()
-					}
-				</ScrollableTabView>
+			<View style={styles.container} id="mainView">
+				<View style={styles.nav_container}>
+					<Button
+						onPress={() => {
+							this.setState({
+									index: nextPage
+								}
+							)
+						}}
+						title={"Next chapter (" + nextPage + "/" + this.pages.length + ")"}  // todo: The next page number to display is different then the next page. if nextPage is always one less the display next page
+						color="#841584"
+						accessibilityLabel="Next Chapter"
+						style={styles.nav_buttons}
+					/>
+					<Button
+						onPress={() => {
+							this.setState({
+									index: previousPage
+								}
+							)
+						}}
+						title={"Previous chapter (" + previousPage + "/" + this.pages.length + ")"}
+						color="#841584"
+						accessibilityLabel="Previous Chapter"
+						style={styles.nav_buttons}
+					/>
+				</View>
+				<View style={styles.content_container}>
+					<Text>{this.state.index}</Text>
+					<WebView source={this.pages[this.state.index].file} originWhitelist={['*']} style={styles.webview}   injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=width, initial-scale=0.7, maximum-scale=2, user-scalable=2.0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
+  scalesPageToFit={false}> </WebView>
+				</View>
 			</View>
-		);
+		)
+			;
 	}
 }
 
@@ -92,12 +116,25 @@ const styles = StyleSheet.create({
 		flex: 1,
 		flexDirection: "column",
 		justifyContent: "center",
-		alignItems: "flex-start"
+		// alignItems: "flex-start"
+	},
+	nav_container: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "center",
+		// alignItems: "flex-start"
+	},
+	nav_buttons: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+		// alignItems: "flex-start"
+	},
+	content_container: {
+		flex: 9,
 	},
 	webview: {
 		flex: 1,
-		flexDirection: "column",
-		height: 200
 	}
 });
 
